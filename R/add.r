c7a39f9ba248677367dfc900e1d62da3a7791b4d
andres cifuentes <andresm12@gmail.com>
2023-02-21 11:22:15 +1030

    third commit

---
+++ R/add.r
@@ -0,0 +1,14 @@
+#' Add together two numbers
+#'
+#' @param x a numeric value
+#' @param y a nuemric value
+#' @return the sum of the two provided  parameters
+#' @examples
+#' add(10,13)
+#' add(12,-34)
+#'
+
+
+add <- function(x,y){
+  return(x+y)
+}
